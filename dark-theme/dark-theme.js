86267e78b9cad236c4e3993dbc6e56950b782100
Nathan Nichols <nwnichols02@gmail.com>
2022-01-31 15:33:16 -0700

    completed the dark theme portion

--- dark-theme/dark-theme.js
+++ dark-theme/dark-theme.js
@@ -1,1 +1,6 @@
 // you need to create DOM selectors to grab the correct HTML elements and update their styles to the opposite theme's css classes when you want to toggle the theme off and on
+
+function myFunction() {
+    const element = document.body;
+    element.classList.toggle("dark-mode");
+}
